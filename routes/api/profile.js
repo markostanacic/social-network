abe9a69e481913a79995f964f624d2d02acfffa9
markostanacic <marko.stanacic@gmail.com>
2018-11-25 00:18:06 +0100

    [no-issue] => added user model (api + validation and registration)

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,6 @@ const router = express.Router();
 // @route   GET api/profile/test
 // @desc    Tests profile routes
 // @access  Public
-router.get('/test', (req, res) => res.json({msg: "Profile works"}));
+router.get('/test', (req, res) => res.json({msg: 'Profile works'}));
 
 module.exports = router;
